32e70bdaf546e625fd010d747e384fa609c45646
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:50 +0000

    Add support for mjs files (ES Modules)

---
+++ js/keyTable.semanticui.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-keytable";export default DataTable;
---
+++ js/keyTable.semanticui.mjs
@@ -0,0 +1,13 @@
+
+/*! Semanic UI styling wrapper for KeyTable
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-se';
+import DataTable from 'datatables.net-keytable';
+
+
+
+
+export default DataTable;
