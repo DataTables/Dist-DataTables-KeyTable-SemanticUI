d59b1e6d45949e9c1a85697c5dc719bc22932966
dtbuild <dtbuild@rigil>
2026-09-22 10:41:36 +0000

    1297ad8540ebfaa842d9c5422a28d2de8f1ef309 Release 3.1.0
    Sync to source repo @1297ad8540ebfaa842d9c5422a28d2de8f1ef309

--- js/keyTable.semanticui.min.mjs
+++ js/keyTable.semanticui.min.mjs
@@ -1,4 +1,4 @@
-/*! KeyTable Fomantic styling 3.0.0 for DataTables
+/*! KeyTable Fomantic styling 3.1.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-se";import KeyTable from"datatables.net-keytable";export default DataTable;
